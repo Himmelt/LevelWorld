ca1aa1cd6df35863b608044a97e99e5e79085a4a
Himmelt <master@void-3.cn>
2018-08-16 13:19:31 +0800

    remove LocationSerializer

--- src/main/java/org/soraworld/lvlworld/LevelManager.java
+++ src/main/java/org/soraworld/lvlworld/LevelManager.java
@@ -1,23 +1,17 @@
 package org.soraworld.lvlworld;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
-import org.soraworld.hocon.node.*;
-import org.soraworld.hocon.serializer.TypeSerializer;
+import org.soraworld.hocon.node.Setting;
 import org.soraworld.violet.api.IPlugin;
 import org.soraworld.violet.manager.VioletManager;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.lang.reflect.Type;
 import java.nio.file.Path;
 import java.util.HashMap;
-
-import static org.bukkit.util.NumberConversions.toDouble;
-import static org.bukkit.util.NumberConversions.toFloat;
 
 public class LevelManager extends VioletManager {
 
@@ -30,33 +24,6 @@ public class LevelManager extends VioletManager {
 
     public LevelManager(IPlugin plugin, Path path) {
         super(plugin, path);
-        options.getSerializers().registerType(Location.class, new TypeSerializer<Location>() {
-            @Override
-            public Location deserialize(@Nonnull Type type, @Nonnull Node node) {
-                if (node instanceof NodeMap) {
-                    HashMap<String, String> map = ((NodeMap) node).asStringMap();
-                    World world = Bukkit.getWorld(map.get("world"));
-                    if (world != null) {
-                        return new Location(world, toDouble(map.get("x")), toDouble(map.get("y")), toDouble(map.get("z")), toFloat(map.get("yaw")), toFloat(map.get("pitch")));
-                    }
-                }
-                return null;
-            }
-
-            @Override
-            public Node serialize(@Nonnull Type type, Location loc, @Nonnull NodeOptions options) {
-                if (loc != null) {
-                    NodeMap map = new NodeMap(options);
-                    map.setNode("world", loc.getWorld().getName());
-                    map.setNode("x", loc.getX());
-                    map.setNode("y", loc.getY());
-                    map.setNode("z", loc.getZ());
-                    map.setNode("yaw", loc.getYaw());
-                    map.setNode("pitch", loc.getPitch());
-                    return map;
-                } else return new NodeBase(options, null, false);
-            }
-        });
     }
 
     @Nonnull
